ba69917b0790a29ca2ee52cd9bff9ba79bf35789
zviko163 <shammah.dzwairo@ashesi.edu.gh>
2026-03-25 12:57:40 +0000

    image edits

--- zaoga-youth-mvurwi/src/pages/WhoAreWe.tsx
+++ zaoga-youth-mvurwi/src/pages/WhoAreWe.tsx
@@ -13,7 +13,7 @@ const advisers = [
 
 const executiveBoard = [
   { role: "Chairman", name: "Brother Carlton", image: "/images/board_checks.jpeg" },
-  { role: "Vice Chair", name: "Sister Joane", image: "/images/board_vice_chair.jpeg" },
+  { role: "Vice Chair", name: "Sister Joane", image: "/images/board_vice_chairlady.jpeg" },
   { role: "Secretary", name: "Sister Getrude", image: "/images/board_getty.jpeg" }
 ];
 
