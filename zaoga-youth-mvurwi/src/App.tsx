e86104ed16f592065e1e6b202f33afea393673f4
zviko163 <shammah.dzwairo@ashesi.edu.gh>
2026-03-25 10:38:51 +0000

    fixing issues with images and rendering

--- zaoga-youth-mvurwi/src/App.tsx
+++ zaoga-youth-mvurwi/src/App.tsx
@@ -10,37 +10,39 @@ import WhoWeAre from './pages/WhoAreWe';
 
 function App() {
   return (
-    <Router>
-      {/* The main wrapper for the whole site */}
-      <div className="flex flex-col min-h-screen bg-brand-white selection:bg-yellow-300 selection:text-brand-primary">
-        
-        {/* The Navbar stays at the top of every page */}
-        <Navbar />
-        
-        {/* The <main> tag is where the pages swap out based on the URL */}
-        <main className="flex-grow">
-          <Routes>
-            {/* The actual pages we have built */}
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/who-we-are" element={<WhoWeAre />} />
-            <Route path="/programs" element={<Programs />} />
-            <Route 
-              path="/events" 
-              element={<Events />} 
-            />
-            <Route 
-              path="/contact" 
-              element={<div className="flex items-center justify-center h-screen text-2xl font-bold text-brand-primary">Contact Page Coming Soon...</div>} 
-            />
-            <Route path="/gallery" element={<Gallery />} />
-          </Routes>
-        </main>
-        
-        <Footer />
-        
-      </div>
-    </Router>
+    <div className="overflow-x-hidden w-full max-w-[100vw] min-h-screen flex flex-col">
+      <Router>
+        {/* The main wrapper for the whole site */}
+        <div className="flex flex-col min-h-screen bg-brand-white selection:bg-yellow-300 selection:text-brand-primary">
+          
+          {/* The Navbar stays at the top of every page */}
+          <Navbar />
+          
+          {/* The <main> tag is where the pages swap out based on the URL */}
+          <main className="flex-grow">
+            <Routes>
+              {/* The actual pages we have built */}
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/who-we-are" element={<WhoWeAre />} />
+              <Route path="/programs" element={<Programs />} />
+              <Route 
+                path="/events" 
+                element={<Events />} 
+              />
+              <Route 
+                path="/contact" 
+                element={<div className="flex items-center justify-center h-screen text-2xl font-bold text-brand-primary">Contact Page Coming Soon...</div>} 
+              />
+              <Route path="/gallery" element={<Gallery />} />
+            </Routes>
+          </main>
+          
+          <Footer />
+          
+        </div>
+      </Router>
+    </div>
   );
 }
 
